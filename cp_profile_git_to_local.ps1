b8002a0a8bb736fa24c5a052693a5e6009a21a70
Jakob F. Gregersen <jfg@bluepp.dk>
2021-12-10 18:18:43 +0100

    Add (profile.ps1): Added function to copy from git. Changed name of cp_profile. Reflected changes in profile.ps1

---
+++ cp_profile_git_to_local.ps1
@@ -0,0 +1,3 @@
+# This script copies github repository into the Powershell profile 
+
+Copy-Item -Path "$($psh_repo)\profile\" -Destination "C:\Users\JakobFaarbaekGregers\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
--- profile/Microsoft.PowerShell_profile.ps1
+++ profile/Microsoft.PowerShell_profile.ps1
@@ -7,7 +7,8 @@ $psh_repo = 'C:\Users\JakobFaarbaekGregers\Github\powershell-scripts\';
 $git_repo = 'C:\Users\JakobFaarbaekGregers\Github\';
 
 Set-Alias -Name "work" -Value "$($psh_repo)work.ps1" -Description 'This runs the script work.ps1, from the Github folder.';
-Set-Alias -Name "cp_profile" -Value "$($psh_repo)cp_profile.ps1" -Description 'This runs the script cp_profile, that copies the powershell profile into the github folder.'
+Set-Alias -Name "cp_to_local" -Value "$($psh_repo)cp_profile_git_to_local.ps1" -Description 'This runs the script cp_to_local, that copies the powershell profile into the local folder.'
+Set-Alias -Name "cp_to_git" -Value "$($psh_repo)cp_profile_local_to_git.ps1" -Description 'This runs the script cp_to_git, that copies the powershell profile into the github folder.'
 
 function nm {
     npm start
