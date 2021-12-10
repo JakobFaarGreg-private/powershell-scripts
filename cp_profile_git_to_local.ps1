a3c9efdba8024aa6b1d58085a5168867007bad8f
Jakob F. Gregersen <jfg@bluepp.dk>
2021-12-10 18:30:00 +0100

    Fix (cp_to_local): Fixed error with Copy-Item used on a folder..

--- cp_profile_git_to_local.ps1
+++ cp_profile_git_to_local.ps1
@@ -1,3 +1,3 @@
 # This script copies github repository into the Powershell profile 
 
-Copy-Item -Path "$($psh_repo)\profile\" -Destination "C:\Users\JakobFaarbaekGregers\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+Copy-Item -Path "$($psh_repo)\profile\Microsoft.PowerShell_profile.ps1" -Destination "C:\Users\JakobFaarbaekGregers\Documents\WindowsPowerShell\"
