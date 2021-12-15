69123531a6fa2e01d7ff282a044628814cce5879
Jakob F. Gregersen <jfg@bluepp.dk>
2021-12-15 15:57:28 +0100

    Add (work.ps1): Creates terminal window for PtA-Hoest

--- work.ps1
+++ work.ps1
@@ -7,3 +7,4 @@ Write-Output "Opening API, Model-R and Excel Add-in";
 wt --window 0 new-tab -d "$($git_repo)ptx-esbjerg-api-r" --title API --tabColor "#FFD700";
 wt --window 0 new-tab -d "$($git_repo)ptx-esbjerg-model-r" --title Model --tabColor "#00BFFF";
 wt --window 0 new-tab -d "$($git_repo)ptx-esbjerg-excel-addin" --title "Excel Add-in" --tabColor "#3CB371";
+wt --window 0 new-tab -d "$($git_repo)PtA-Hoest" --title PtA-Hoest --tabColor "#C0C0C0";
